6c70f3cff466bdfcac093ba1af5ee9b0d1c25977
Davide <davide.batista@hotmail.it>
2024-10-09 12:52:41 +0200

    Add AI_Folder
    Added AI_Folder and removed all build subfolders insider actuator/sensors implementation

--- Implementation/Project_SmartPowerMeter/build/cooja/obj/.deps/math_utilities.c.d
+++
@@ -1,6 +0,0 @@
-build/cooja/obj/math_utilities.o: ../Project_Utilities/math_utilities.c \
- ../../os/net/app-layer/coap/module-macros.h \
- ../../os/services/shell/module-macros.h ../../os/lib/random.h
-../../os/net/app-layer/coap/module-macros.h:
-../../os/services/shell/module-macros.h:
-../../os/lib/random.h:
